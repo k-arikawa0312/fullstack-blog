07634f5bb40bd6a7c6e6d9e318abe68f9a0ca4f4
k-arikawa0312 <s1F102402710@iniad.org>
2024-07-30 19:03:00 +0900

    ブログの詳細記事取得API

---
+++ src/app/api/blog/[id]/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import { main } from "../route";
+import { PrismaClient } from "@prisma/client/extension";
+
+const prisma =new PrismaClient();
+
+export const GET =async (req:Request,res :NextResponse)=>{
+    try {
+        const id:number = parseInt(req.url.split("/blog/")[1]);
+        await main()
+        const posts = await prisma.post.findFirst({where:{id}})
+        return NextResponse.json({message:'Success',posts},{status:200})
+    }catch (err){
+        return NextResponse.json({message:'Error',err},{status:500})
+    } finally{
+        await prisma.$disconnect()
+    }
+};
