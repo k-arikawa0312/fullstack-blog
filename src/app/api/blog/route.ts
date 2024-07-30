c7f6f2b4e4990b7dadc6895bd967b9273cdfdc56
k-arikawa0312 <s1F102402710@iniad.org>
2024-07-30 18:55:59 +0900

    記事投稿API作成

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -20,6 +20,20 @@ export const GET =async (req:Request,res :NextResponse)=>{
     }catch (err){
         return NextResponse.json({message:'Error',err},{status:500})
     } finally{
+        await prisma.$disconnect()
+    }
+};
 
+//ブログ投稿API
+export const POST =async (req:Request,res :NextResponse)=>{
+    try {
+        const {title,description}=await req.json()
+        await main()
+        const posts = await prisma.post.create({data:{title,description}})
+        return NextResponse.json({message:'Success',posts},{status:201})
+    }catch (err){
+        return NextResponse.json({message:'Error',err},{status:500})
+    } finally{
+        await prisma.$disconnect()
     }
 };
