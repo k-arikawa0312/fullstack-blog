0571430163c6ccdfb108351da61ea9ae07aa82b3
k-arikawa0312 <s1F102402710@iniad.org>
2024-07-30 20:37:50 +0900

    記事一覧表示

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -36,3 +36,21 @@ export const PUT =async (req:Request,res :NextResponse)=>{
         await prisma.$disconnect()
     }
 };
+
+export const DELETE =async (req:Request,res :NextResponse)=>{
+    try {
+        const id:number = parseInt(req.url.split("/blog/")[1]);
+
+        
+        await main()
+        const post =await prisma.post.delete({
+            where:{id},
+        })
+        const posts = await prisma.post.findFirst({where:{id}})
+        return NextResponse.json({message:'Success',posts},{status:200})
+    }catch (err){
+        return NextResponse.json({message:'Error',err},{status:500})
+    } finally{
+        await prisma.$disconnect()
+    }
+};
