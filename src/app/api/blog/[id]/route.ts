6eae389f3bf3cbc28e70d2684d124214c3092158
k-arikawa0312 <s1F102402710@iniad.org>
2024-07-30 19:07:57 +0900

    記事編集API

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -16,3 +16,23 @@ export const GET =async (req:Request,res :NextResponse)=>{
         await prisma.$disconnect()
     }
 };
+
+
+export const PUT =async (req:Request,res :NextResponse)=>{
+    try {
+        const id:number = parseInt(req.url.split("/blog/")[1]);
+
+        const {title,description}= await req.json()
+        await main()
+        const post =await prisma.post.update({
+            data:{title,description},
+            where:{id},
+        })
+        const posts = await prisma.post.findFirst({where:{id}})
+        return NextResponse.json({message:'Success',posts},{status:200})
+    }catch (err){
+        return NextResponse.json({message:'Error',err},{status:500})
+    } finally{
+        await prisma.$disconnect()
+    }
+};
