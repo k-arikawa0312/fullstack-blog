c246d4270da0499b717fc36c307910fe8f76d9ac
k-arikawa0312 <s1F102402710@iniad.org>
2024-08-01 20:07:46 +0900

    いったん完成

--- src/app/blog/edit/[id]/page.tsx
+++ src/app/blog/edit/[id]/page.tsx
@@ -1,85 +1,121 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-"use client"
+"use client";
+
 import { useRouter } from "next/navigation";
-import React, { useEffect, useRef } from "react";
-import toast, { Toaster } from "react-hot-toast";
+import React, { Fragment, useEffect, useRef } from "react";
+import { Toaster, toast } from "react-hot-toast";
 
+type UpdateBlogParams = {
+  title: string;
+  description: string;
+  id: string;
+};
 
+const updateBlog = async (data: UpdateBlogParams) => {
+  const res = fetch(`http://localhost:3020/api/blog/${data.id}`, {
+    method: "PUT",
+    body: JSON.stringify({ title: data.title, description: data.description }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return (await res).json();
+};
 
-const editBlog=async(title:string|undefined,description:string|undefined,id:number)=>{
-    
-    const res = await fetch(`http://localhost:3020/blog/edit/${id}`,{
-       method:"PUT",
-       headers:{
-           "Content-type":"application/json"
-       },
-       body:JSON.stringify({title,description,id}),
-    })
-    return res.json();
-   }
-  
+const getBlogById = async (id: number) => {
+  const res = await fetch(`http://localhost:3020/api/blog/${id}`);
+  const data = await res.json();
+  return data.post;
+};
 
-const getBlogById=async(id:number)=>{
-    
-    const res = await fetch(`http://localhost:3020/blog/edit/${id}`)     
-    const data=await res.json();
-    return data.post;
-   }
-      
-   
-   
-   const page=({params}:{params:{id:number}})=>{
-       
-       const router=useRouter()
-       
-       const titleref=useRef<HTMLInputElement|null>(null);
-       const descriptionref=useRef<HTMLTextAreaElement|null>(null);
-       
-    useEffect(()=>{
-        getBlogById(params.id).then((data)=>{
-            titleref.current!.value=data.title
-            descriptionref.current!.value=data.description
-        }).catch(err=>{
-            toast.error("エラー発生",{id:"1"})
-        })
-    },[])
-    const handleSubmit=async(e:React.FormEvent)=>{
-        e.preventDefault();
-        toast.loading('編集です',{id:"1"})
-        await editBlog(titleref.current?.value,descriptionref.current?.value,params.id)
-        toast.success("編集完了",{id:"1"})
-        router.push("/")
-        router.refresh()
+const deleteBlog = async (id: number) => {
+  const res = fetch(`http://localhost:3020/api/blog/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return (await res).json();
+};
+
+const EditBlog = ({ params }: { params: { id: string } }) => {
+  const router = useRouter();
+  const titleRef = useRef<HTMLInputElement | null>(null);
+  const descriptionRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (titleRef.current && descriptionRef.current) {
+      toast.loading("Sending Request 🚀", { id: "1" });
+
+      await updateBlog({
+        title: titleRef.current?.value,
+        description: descriptionRef.current?.value,
+        id: params.id,
+      });
+
+      toast.success("Blog Posted Successfully", { id: "1" });
+
+      router.push("/");
+      router.refresh();
     }
-    return (
-        <>
-        <Toaster></Toaster>
-        <div className="w-full m-auto flex my-4">
-            <div className="flex flex-col justify-center items-center m-auto">
-            <p className="text-2xl text-slate-200 font-bold p-3">ブログの編集 🚀</p>
-            <form onSubmit={handleSubmit}>
-                <input
-                ref={titleref}
-                placeholder="タイトルを入力"
-                type="text"
-                className="rounded-md px-4 w-full py-2 my-2"
-                />
-                <textarea
-                ref={descriptionref}
-                placeholder="記事詳細を入力"
-                className="rounded-md px-4 py-2 w-full my-2"
-                ></textarea>
-                <button className="font-semibold px-4 py-2 shadow-xl bg-slate-200 rounded-lg m-auto hover:bg-slate-100">
-                更新
-                </button>
-                <button className="ml-2 font-semibold px-4 py-2 shadow-xl bg-red-400 rounded-lg m-auto hover:bg-slate-100">
-                削除
-                </button>
-            </form>
-            </div>
-        </div>
-        </>
-)
-}
+  };
 
-export default page;
+  const handleDelete = async () => {
+    toast.loading("Deleting Blog", { id: "2" });
+    await deleteBlog(parseInt(params.id));
+  };
+
+  useEffect(() => {
+    toast.loading("Fetching Blog Details 🚀", { id: "1" });
+    getBlogById(parseInt(params.id))
+      .then((data) => {
+        if (titleRef.current && descriptionRef.current) {
+          titleRef.current.value = data.title;
+          descriptionRef.current.value = data.description;
+          toast.success("Fetching Completed", { id: "1" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error("Error Fetching Blog", { id: "1" });
+      });
+  }, []);
+
+  return (
+    <>
+      <Toaster />
+      <div className="w-full m-auto flex my-4">
+        <div className="flex flex-col justify-center items-center m-auto">
+          <p className="text-2xl text-slate-200 font-bold p-3">
+            Edit a Wonderful Blog 🚀
+          </p>
+          <form onSubmit={handleSubmit}>
+            <input
+              ref={titleRef}
+              placeholder="Enter Title"
+              type="text"
+              className="rounded-md px-4 w-full py-2 my-2"
+            />
+            <textarea
+              ref={descriptionRef}
+              placeholder="Enter Description"
+              className="rounded-md px-4 py-2 w-full my-2"
+            ></textarea>
+            <button className="font-semibold px-4 py-2 shadow-xl bg-slate-200 rounded-lg m-auto hover:bg-slate-100">
+              Update
+            </button>
+            <button
+              onClick={handleDelete}
+              className="ml-2 font-semibold px-4 py-2 shadow-xl bg-red-400 rounded-lg m-auto hover:bg-slate-100"
+            >
+              Delete
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EditBlog;
