38ea3eb3d3078f7dfbf6602c2c11b67c61b5f169
k-arikawa0312 <s1F102402710@iniad.org>
2024-08-23 01:25:05 +0900

    redeploy

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -1,65 +1,64 @@
 import { NextResponse } from "next/server";
-import { main } from "../route";
 import { PrismaClient } from "@prisma/client";
 
-const prisma =new PrismaClient();
+const prisma = new PrismaClient();
 
 export const GET = async (req: Request, res: NextResponse) => {
-    try {
-        console.log(25)
-      const id: number = parseInt(req.url.split("/blog/")[1]);
-      await main();
-  
-      const post = await prisma.post.findFirst({ where: { id : id } });
-  
-      if (!post) {
-        return NextResponse.json({ message: "Not Found" }, { status: 404 });
-      }
-  
-      return NextResponse.json({ message: "Success", post }, { status: 200 });
-    } catch (err) {
-        console.log(8599)
-      return NextResponse.json({ message: "Error", err }, { status: 500 });
-    } finally {
-        console.log(99999999)
-      await prisma.$disconnect();
+  try {
+    console.log(25);
+    const id: number = parseInt(req.url.split("/blog/")[1]);
+    await prisma.$connect();
+
+    const post = await prisma.post.findFirst({ where: { id: id } });
+
+    if (!post) {
+      return NextResponse.json({ message: "Not Found" }, { status: 404 });
     }
-  };
-  
-  export const PUT = async (req: Request, res: NextResponse) => {
-    try {
-      const id: number = parseInt(req.url.split("/blog/")[1]);
-      const { title, description } = await req.json();
-  
-      await main();
-  
-      const post = await prisma.post.update({
-        data: { title, description },
-        where: { id },
-      });
-  
-      return NextResponse.json({ message: "Success", post }, { status: 200 });
-    } catch (err) {
-      return NextResponse.json({ message: "Error", err }, { status: 500 });
-    } finally {
-      await prisma.$disconnect();
-    }
-  };
-  
-  export const DELETE = async (req: Request, res: NextResponse) => {
-    try {
-      const id: number = parseInt(req.url.split("/blog/")[1]);
-  
-      await main();
-  
-      const post = await prisma.post.delete({
-        where: { id },
-      });
-  
-      return NextResponse.json({ message: "Success", post }, { status: 200 });
-    } catch (err) {
-      return NextResponse.json({ message: "Error", err }, { status: 500 });
-    } finally {
-      await prisma.$disconnect();
-    }
-  };
+
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (err) {
+    console.log(8599);
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    console.log(99999999);
+    await prisma.$disconnect();
+  }
+};
+
+export const PUT = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split("/blog/")[1]);
+    const { title, description } = await req.json();
+
+    await prisma.$connect();
+
+    const post = await prisma.post.update({
+      data: { title, description },
+      where: { id },
+    });
+
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+export const DELETE = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split("/blog/")[1]);
+
+    await prisma.$connect();
+
+    const post = await prisma.post.delete({
+      where: { id },
+    });
+
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function main(): Promise<void> {
+async function main(): Promise<void> {
   await prisma.$connect();
 }
 
