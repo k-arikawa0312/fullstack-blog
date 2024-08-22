2bf04c862dec6a769976da5d7545683dd6e32301
k-arikawa0312 <s1F102402710@iniad.org>
2024-08-23 01:21:12 +0900

    redeploy

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function main(): Promise<void | never> {
+async function main(): Promise<void> {
   await prisma.$connect();
 }
 
