20b0853e50b431aba1fde44b87246587e538fad6
k-arikawa0312 <s1F102402710@iniad.org>
2024-08-23 01:55:04 +0900

    redeploy

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -1,64 +1,28 @@
-import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
 
-const prisma = new PrismaClient();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { id } = req.query;
 
-export const GET = async (req: Request, res: NextResponse) => {
-  try {
-    console.log(25);
-    const id: number = parseInt(req.url.split("/blog/")[1]);
-    await prisma.$connect();
-
-    const post = await prisma.post.findFirst({ where: { id: id } });
-
-    if (!post) {
-      return NextResponse.json({ message: "Not Found" }, { status: 404 });
-    }
-
-    return NextResponse.json({ message: "Success", post }, { status: 200 });
-  } catch (err) {
-    console.log(8599);
-    return NextResponse.json({ message: "Error", err }, { status: 500 });
-  } finally {
-    console.log(99999999);
-    await prisma.$disconnect();
-  }
-};
-
-export const PUT = async (req: Request, res: NextResponse) => {
-  try {
-    const id: number = parseInt(req.url.split("/blog/")[1]);
-    const { title, description } = await req.json();
-
-    await prisma.$connect();
-
-    const post = await prisma.post.update({
-      data: { title, description },
-      where: { id },
+  if (req.method === "GET") {
+    const response = await fetch(`http://localhost:3020/api/blog/${id}`);
+    const blog = await response.json();
+    res.status(200).json(blog);
+  } else if (req.method === "PUT") {
+    const { title, description } = req.body;
+    const response = await fetch(`http://localhost:3020/api/blog/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, description }),
     });
-
-    return NextResponse.json({ message: "Success", post }, { status: 200 });
-  } catch (err) {
-    return NextResponse.json({ message: "Error", err }, { status: 500 });
-  } finally {
-    await prisma.$disconnect();
+    const updatedBlog = await response.json();
+    res.status(200).json(updatedBlog);
+  } else {
+    res.setHeader("Allow", ["GET", "PUT"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
-};
-
-export const DELETE = async (req: Request, res: NextResponse) => {
-  try {
-    const id: number = parseInt(req.url.split("/blog/")[1]);
-
-    await prisma.$connect();
-
-    const post = await prisma.post.delete({
-      where: { id },
-    });
-
-    return NextResponse.json({ message: "Success", post }, { status: 200 });
-  } catch (err) {
-    return NextResponse.json({ message: "Error", err }, { status: 500 });
-  } finally {
-    await prisma.$disconnect();
-  }
-};
+}
