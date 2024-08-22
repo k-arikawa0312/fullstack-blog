8e75eb28b44a21be655ae7a80de15264789e09e0
k-arikawa0312 <s1F102402710@iniad.org>
2024-08-23 02:01:39 +0900

    redeploy

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -1,34 +1,40 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
 ) {
-  const { id } = req.query;
+  const { id } = params;
+  const response = await fetch(`http://localhost:3020/api/blog/${id}`);
+  const blog = await response.json();
+  return NextResponse.json(blog);
+}
 
-  if (req.method === "GET") {
-    const response = await fetch(`http://localhost:3020/api/blog/${id}`);
-    const blog = await response.json();
-    res.status(200).json(blog);
-  } else if (req.method === "PUT") {
-    const { title, description } = req.body;
-    const response = await fetch(`http://localhost:3020/api/blog/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ title, description }),
-    });
-    const updatedBlog = await response.json();
-    res.status(200).json(updatedBlog);
-  } else if (req.method === "DELETE") {
-    const response = await fetch(`http://localhost:3020/api/blog/${id}`, {
-      method: "DELETE",
-    });
-    const deletedBlog = await response.json();
-    res.status(200).json(deletedBlog);
-  } else {
-    res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const { title, description } = await req.json();
+  const response = await fetch(`http://localhost:3020/api/blog/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title, description }),
+  });
+  const updatedBlog = await response.json();
+  return NextResponse.json(updatedBlog);
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const response = await fetch(`http://localhost:3020/api/blog/${id}`, {
+    method: "DELETE",
+  });
+  const deletedBlog = await response.json();
+  return NextResponse.json(deletedBlog);
 }
