8bc8e195b3124f32a5a894da5562b6876fdc623e
k-arikawa0312 <s1F102402710@iniad.org>
2024-08-23 01:16:37 +0900

    fix

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -3,12 +3,8 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function main() {
-  try {
-    await prisma.$connect();
-  } catch (err) {
-    return Error("DB接続に失敗しました");
-  }
+export async function main(): Promise<void | never> {
+  await prisma.$connect();
 }
 
 export const GET = async (req: Request, res: NextResponse) => {
